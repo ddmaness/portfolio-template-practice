44b501c9862e5cd95bb8eec71135b797920fb3f6
Donald Maness <ddmaness@gmail.com>
2017-12-06 18:16:30 -0500

    Make nav display active part of page

--- portfolio.js
+++ portfolio.js
@@ -9,3 +9,43 @@ function dropNav() {
 	}
 }
 
+function revealProjects() {
+	var loadMoreText = document.getElementsByClassName('load-more-text');
+	var halfList = document.getElementsByClassName('responsive-list');
+	if (halfList[1].classList.contains('hidden-projects')) {
+		halfList[1].classList.remove('hidden-projects');
+		loadMoreText[0].innerHTML = 'HIDE PROJECTS';
+	}
+	else {
+		loadMoreText[0].innerHTML = 'LOAD MORE PROJECTS';
+		halfList[0].scrollIntoView();
+		halfList[1].classList.add('hidden-projects');
+	}
+}
+
+function activeNavHighlight() {
+	var items = document.getElementsByClassName('fixed-nav-bar')[0].getElementsByTagName('li');
+	var sections = [
+		document.getElementsByClassName('about')[0],
+		document.getElementsByClassName('project-list')[0],
+		document.getElementsByClassName('work')[0],
+		document.getElementsByClassName('service-list')[0],
+		document.getElementsByClassName('testimonial-background')[0],
+		document.getElementsByClassName('contact')[0]
+	];
+	var currentY = self.pageYOffset;
+	sections.forEach(function(elem, index, arr) {
+		if (index == arr.length - 1 && currentY > elem.offsetTop) {
+			items[index + 1].style.color = '#EDE7F6';
+			return;
+		}
+		if (currentY > elem.offsetTop && currentY < arr[index + 1].offsetTop ){
+			items[index + 1].style.color = '#EDE7F6';
+			return;
+		}
+		else {
+			items[index + 1].style.color = '#424242';
+			return;
+		}
+	});
+}
